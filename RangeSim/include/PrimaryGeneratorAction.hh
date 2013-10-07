811dff71029653ac000b61a735d5490259d68a85
Matthew Urffer <matthew.urffer@gmail.com>
2013-10-07 11:40:58 -0400

    Using the GPS for the primary particle
    The code compiles, but seg faults.

--- RangeSim/include/PrimaryGeneratorAction.hh
+++ RangeSim/include/PrimaryGeneratorAction.hh
@@ -2,37 +2,19 @@
 #define PrimaryGeneratorAction_h 1
 
 #include "G4VUserPrimaryGeneratorAction.hh"
-#include "G4ParticleGun.hh"
-#include "globals.hh"
 
+class G4ParticleGun;
+class G4GeneralParticleSource;
 class G4Event;
-class DetectorConstruction;
-class PrimaryGeneratorMessenger;
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
 {
-  public:
-    PrimaryGeneratorAction(DetectorConstruction*);    
-   ~PrimaryGeneratorAction();
+public:
+  PrimaryGeneratorAction();    
+  virtual ~PrimaryGeneratorAction();
+  virtual void GeneratePrimaries(G4Event* event);
 
-  public:
-    void SetDefaultKinematic(G4int);
-    void SetRndmBeam(G4double val)  {fRndmBeam = val;}   
-    virtual void GeneratePrimaries(G4Event*);
-    
-    G4ParticleGun* GetParticleGun() {return fParticleGun;}
-
-  private:
-    G4ParticleGun*             fParticleGun;
-    DetectorConstruction*      fDetector;
-    G4double                   fRndmBeam;       
-    PrimaryGeneratorMessenger* fGunMessenger;     
+private:
+  G4GeneralParticleSource*  fParticleGun; // G4 particle gun
 };
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
 #endif
-
-
