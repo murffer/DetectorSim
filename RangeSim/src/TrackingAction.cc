f79bcb92634facdfa4368579dd6b473db152d0af
Matthew J. Urffer <matthew.urffer@gmail.com>
2013-10-07 23:28:45 -0400

    Updated documentation of the tracking manager
    Updated the documentation of the tracking manager

--- RangeSim/src/TrackingAction.cc
+++ RangeSim/src/TrackingAction.cc
@@ -5,40 +5,41 @@
 
 #include "G4Track.hh"
 #include "G4GeneralParticleSource.hh"
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-TrackingAction::TrackingAction(PrimaryGeneratorAction* prim, RunAction* run)
+/**
+ * Creates a hook into the tracking action
+ */
+  TrackingAction::TrackingAction(PrimaryGeneratorAction* prim, RunAction* run)
 :fPrimary(prim), fRunAction(run)
 { }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
+/**
+ * Nothing is completed
+ */
 void TrackingAction::PreUserTrackingAction(const G4Track*)
-{
-  //  G4cout << "ID= " << aTrack->GetTrackID() << "  e(MeV)= " 
-  //         << aTrack->GetDynamicParticle()->GetKineticEnergy()/MeV << "  "
-  //         << aTrack->GetDynamicParticle()->GetDefinition()->GetParticleName()
-  //         << G4endl;    
-}
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
+{ }
+/**
+ * Computes the true and projected range. The track length is computed, and the
+ * number of steps for the track are incremented in the histograms through 
+ * G4AnalysisManager.
+ *
+ * @param aTrack - the track
+ */
 void TrackingAction::PostUserTrackingAction(const G4Track* aTrack)
 {  
   //increase nb of processed tracks 
   //count nb of steps of this track
   G4int   nbSteps = aTrack->GetCurrentStepNumber();
   G4double Trleng = aTrack->GetTrackLength();
-    
+
   if (aTrack->GetDefinition()->GetPDGCharge() == 0.) {
     fRunAction->CountTraks0(1); 
     fRunAction->CountSteps0(nbSteps);
-  
+
   } else {
     fRunAction->CountTraks1(1); 
     fRunAction->CountSteps1(nbSteps);
   }
-  
+
   //true and projected ranges for primary particle
   if (aTrack->GetTrackID() == 1) {
     fRunAction->AddTrueRange(Trleng);
@@ -47,12 +48,9 @@ void TrackingAction::PostUserTrackingAction(const G4Track* aTrack)
     fRunAction->AddProjRange(position.x());
     fRunAction->AddTransvDev(position.y());
     fRunAction->AddTransvDev(position.z());
-    
+
     G4AnalysisManager* analysisManager = G4AnalysisManager::Instance();
     analysisManager->FillH1(1,Trleng);
     analysisManager->FillH1(2,(float)nbSteps);        
   }        
 }
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
