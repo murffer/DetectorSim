aae6dbd004f4e77cef1ac6da5ce2e320f25e4b31
Matthew J. Urffer <matthew.urffer@gmail.com>
2013-10-07 23:22:41 -0400

    Updated SteppingAction documentation
    Updated the stepping action documentation of the classes.

--- RangeSim/src/SteppingAction.cc
+++ RangeSim/src/SteppingAction.cc
@@ -1,36 +1,3 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-/// \file electromagnetic/TestEm1/src/SteppingAction.cc
-/// \brief Implementation of the SteppingAction class
-//
-// $Id$
-//
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
 #include "SteppingAction.hh"
 #include "RunAction.hh"
 #include "EventAction.hh"
@@ -39,14 +6,23 @@
 #include "G4SteppingManager.hh"
 #include "G4VProcess.hh"
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
+/**
+ * Creates a hook into a class that is called at every step in the Geant4
+ * simulation. Initilization of the fiels is preformed.
+ *
+ * @param run - the RunAction
+ * @param event - the EventAction
+ */
 SteppingAction::SteppingAction(RunAction* run, EventAction* event)
 :fRunAction(run), fEventAction(event)
 { }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
+/**
+ * Fills the step length and increments the energy depostion. Only the step
+ * length of the primary particle (Track ID = 1) is booked in the histogram.
+ * 
+ * @param aStep - the step
+ */
 void SteppingAction::UserSteppingAction(const G4Step* aStep)
 {
   G4AnalysisManager* analysisManager = G4AnalysisManager::Instance();
@@ -63,7 +39,3 @@ void SteppingAction::UserSteppingAction(const G4Step* aStep)
   G4double steplen = aStep->GetStepLength();
   if (ID == 1) analysisManager->FillH1(3,steplen);  
 }
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-
