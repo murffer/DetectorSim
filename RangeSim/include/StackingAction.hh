d4a71c7385ca292b0bc41f4525366a70bf20b086
Matthew J. Urffer <matthew.urffer@gmail.com>
2013-10-07 23:17:42 -0400

    Updated documentation of StackingAction
    Updated the documentation of stacking action.

--- RangeSim/include/StackingAction.hh
+++ RangeSim/include/StackingAction.hh
@@ -4,19 +4,13 @@
 #include "G4UserStackingAction.hh"
 #include "globals.hh"
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
 class StackingAction : public G4UserStackingAction
 {
   public:
     StackingAction();
-   ~StackingAction();
-     
+    ~StackingAction();
+
     virtual G4ClassificationOfNewTrack ClassifyNewTrack(const G4Track*);
-    
 };
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
 #endif
 
