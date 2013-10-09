a221642777146b4b2c5b8340e9db66e2d364024d
Matthew J. Urffer <matthew.urffer@gmail.com>
2013-10-09 19:48:48 -0400

    Modified to use G4VAnalysiManager (histManager)
    Modified the code to use the G4VAnalysisManager contained in
    histManager while removing uncessary functions.  I think I need to
    voxelize by geometry in replicas in the mother volume, and then create
    an array of Sensitive detectors which I can pass in and then analyaze.

--- EnergyDepTracking/include/Analysis.hh
+++ EnergyDepTracking/include/Analysis.hh
@@ -4,10 +4,6 @@
 #include "G4Event.hh"
 #include "G4Run.hh"
 #include "G4VHitsCollection.hh"
-#include "TH1F.h"
-#include "TH2F.h"
-#include "TNtuple.h"
-#include "TFile.h"
 #include "G4String.hh"
 
 #include "CaloHit.hh"
@@ -32,29 +28,14 @@ class Analysis {
         void EndOfEvent(const G4Event* anEvent);
         void EndOfRun(const G4Run* aRun);
   
-        void SetIncidentParticleName(G4String pName);
-        void SetHistEMax(G4double emax);
-        void SetBinWidth(G4double binWidth);
-        void SetEDepPosAnalysis(G4bool value);
    private:
 
         // Singleton Analysis
         Analysis();
         static Analysis *singleton;
-        G4double GetDetectorThickness();
-        G4double GetCalorimeterThickness();
-        G4String GetDetectorMaterial();
-        G4String incidentParticleName;
         G4double maxHistEnergy;
         G4double posHistBinWidth;
         G4bool EDepPosAnalysis = false;
-
-        // ROOT Output variables
-        TFile* outfile;
-        TH1F* eDepHist;
-        TH2F* posEDepHist;
-        TNtuple* posEDepTuple;
-
 
         // Accumulation Variables
         G4double eDepEvent;
