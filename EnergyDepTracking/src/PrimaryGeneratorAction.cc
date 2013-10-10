df63e0e7e52035ef001884f4275b96f5acbf5c5c
Matthew J. Urffer <matthew.urffer@gmail.com>
2013-10-10 08:34:11 -0400

    Converting to primary generator (simple gun)
    Converting the GPS to a simple gun

--- EnergyDepTracking/src/PrimaryGeneratorAction.cc
+++ EnergyDepTracking/src/PrimaryGeneratorAction.cc
@@ -1,28 +1,66 @@
 #include "PrimaryGeneratorAction.hh"
 
-#include "G4RunManager.hh"
-#include "G4GeneralParticleSource.hh"
+#include "G4LogicalVolumeStore.hh"
+#include "G4LogicalVolume.hh"
+#include "G4Box.hh"
 #include "G4Event.hh"
 #include "G4ParticleGun.hh"
 #include "G4ParticleTable.hh"
 #include "G4ParticleDefinition.hh"
+#include "G4SystemOfUnits.hh"
+
 #include "Randomize.hh"
-#include "globals.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 PrimaryGeneratorAction::PrimaryGeneratorAction()
- : G4VUserPrimaryGeneratorAction(){
-  fParticleGun = new G4GeneralParticleSource();
+ : G4VUserPrimaryGeneratorAction()
+{
+  G4int nofParticles = 1;
+  fParticleGun = new G4ParticleGun(nofParticles);
 
+  // default particle kinematic
+
+  G4ParticleDefinition* particleDefinition 
+    = G4ParticleTable::GetParticleTable()->FindParticle("proton");
+
+  fParticleGun->SetParticleDefinition(particleDefinition);
+  fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0.,0.,1.));
+  fParticleGun->SetParticleEnergy(3.0*GeV);
 }
 
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-PrimaryGeneratorAction::~PrimaryGeneratorAction(){
+PrimaryGeneratorAction::~PrimaryGeneratorAction()
+{
   delete fParticleGun;
 }
 
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 {
+  // This function is called at the begining of event
+
+  // In order to avoid dependence of PrimaryGeneratorAction
+  // on DetectorConstruction class we get world volume
+  // from G4LogicalVolumeStore.
+
+  G4double worldZHalfLength = 0;
+  G4LogicalVolume* worldLV
+    = G4LogicalVolumeStore::GetInstance()->GetVolume("World");
+  G4Box* worldBox = NULL;
+  if ( worldLV ) worldBox = dynamic_cast<G4Box*>(worldLV->GetSolid());
+  if ( worldBox ) worldZHalfLength = worldBox->GetZHalfLength();
+  else  {
+    G4cerr << "World volume of box not found." << G4endl;
+    G4cerr << "Perhaps you have changed geometry." << G4endl;
+    G4cerr << "The gun will be place in the center." << G4endl;
+  }
+
+  fParticleGun->SetParticlePosition(G4ThreeVector(0., 0., -worldZHalfLength));
+
   fParticleGun->GeneratePrimaryVertex(anEvent);
 }
 
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
