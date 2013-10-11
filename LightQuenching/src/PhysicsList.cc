f66dc7d105f2d07458e783a849cac3aa3bec8fc9
Matthew J. Urffer <matthew.urffer@gmail.com>
2013-10-11 17:38:06 -0400

    Removed hadrons from physics list

--- LightQuenching/src/PhysicsList.cc
+++ LightQuenching/src/PhysicsList.cc
@@ -5,7 +5,6 @@
 #include "G4EmStandardPhysics_option4.hh"
 #include "G4EmLivermorePhysics.hh"
 #include "G4OpticalPhysics.hh"
-#include "HadronPhysicsQGSP_BERT_HP.hh"
 #include "G4IonPhysics.hh"
 /**
  * PhysicsList
@@ -13,7 +12,6 @@
  * Constructs the physics of the simulation
  *
  * The physics list relies on modular physics. Currently used modules are:
- *  - Hadronic physics (High Precision)
  *  - IonPhysics
  *  - OpticalPhysics
  *  - EM Standard Physics
@@ -24,11 +22,9 @@ PhysicsList::PhysicsList() : G4VModularPhysicsList() {
     // SetVerboseLevel(1);
 
     // Adding Physics List
-    RegisterPhysics( new HadronPhysicsQGSP_BERT_HP());
     RegisterPhysics( new G4IonPhysics());
     RegisterPhysics( new G4OpticalPhysics());
     RegisterPhysics( new G4EmStandardPhysics_option4());
-   // RegisterPhysics( new G4EmDNAPhysics());
 }
 
 /**
