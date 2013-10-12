63f146f37f05596ab7df05301c16e5665525bb4a
Matthew J. Urffer <matthew.urffer@gmail.com>
2013-10-11 20:00:38 -0400

    Changed phyics list

--- LightQuenching/include/PhysicsList.hh
+++ LightQuenching/include/PhysicsList.hh
@@ -13,14 +13,26 @@ class PhysicsList: public G4VModularPhysicsList
     PhysicsList();
    ~PhysicsList();
 
-    void SetCuts();
+    virtual void ConstructParticle();
+    virtual void ConstructProcess();
+
+    virtual void SetCuts();
+    void SetCutForGamma(G4double);
+    void SetCutForElectron(G4double);
+    void SetCutForPositron(G4double);
+    void SetCutForAlpha(G4double);
+    void SetCutForTriton(G4double);
       
   private:
-    G4double currentDefaultCut;
+    G4double fCutForGamma;        /** Default gamma cut    */
+    G4double fCutForElectron;     /** Default electron cut */
+    G4double fCutForPositron;     /** Default positron cut */
+    G4double fCutForAlpha;        /** Defualt alpha cut    */
+    G4double fCutForTriton;       /** Default triton cut   */
+    G4double fCurrentDefaultCut;  /* Default cut for all particles  */
+    
+    G4VPhysicsConstructor* fEmPhysicsList;  /* Default physics list       */
+    G4String               fEmName;         /* Name of the physics list   */
     
 };
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
 #endif
-
