53aeb63e1c38e5d805b784d95205b36bce86b61e
Matthew Urffer <matthew.urffer@gmail.com>
2013-10-13 12:29:50 -0400

    Number of photons generated

---
+++ LightYeildEDep/src/StackingAction.cc
@@ -0,0 +1,47 @@
+#include "StackingAction.hh"
+#include "G4String.hh"
+#include "G4Track.hh"
+#include "G4ParticleDefinition.hh"
+#include "G4ParticleTypes.hh"
+#include "HistoManager.hh"
+
+StackingAction::StackingAction(){
+   optPhotonCounter = 0;
+}
+
+
+StackingAction::~StackingAction()
+{ }
+/**
+ * NewStage
+ *
+ * Seems to be called once stack is empty, so only after an event is over
+ */
+void StackingAction::NewStage(){
+
+  // Updating Analysis
+  G4AnalysisManager* analysisManager = G4AnalysisManager::Instance();
+  analysisManager->FillH1(1,optPhotonCounter);
+}
+/**
+ * PrepareNewEvent
+ *
+ * Prepares a new event for the stacking action.
+ * The only action we need to do is reset the counters
+ */
+void StackingAction::PrepareNewEvent(){
+  optPhotonCounter = 0;
+}
+/**
+ * ClassifyNewTrack
+ *
+ * Called every time a new track needs to be classified, so we can use it to
+ * determine in the new track is an optical photon
+ */
+G4ClassificationOfNewTrack StackingAction::ClassifyNewTrack(const G4Track* track){
+  // Optical Photon Counter
+  if(track->GetDefinition()== G4OpticalPhoton::OpticalPhotonDefinition()){
+      optPhotonCounter++;
+  }
+  return fUrgent;
+}
