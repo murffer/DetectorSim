4c0e8785d9b59628a8af190996eecfa8b453e7ae
Matthew J. Urffer <matthew.urffer@gmail.com>
2013-10-31 17:02:22 -0400

    Fixed indentation on +/-
    Fixed the spacing on +/- that will mess up an analysis script.

--- ElectronEnergyDeposition/src/RunAction.cc
+++ ElectronEnergyDeposition/src/RunAction.cc
@@ -18,7 +18,7 @@
  * @param det - the geometry of the simulation
  * @param kin - primary generator of this simulation
  */
-RunAction::RunAction(DetectorConstruction* det, PrimaryGeneratorAction* kin)
+  RunAction::RunAction(DetectorConstruction* det, PrimaryGeneratorAction* kin)
 :fDetector(det), fPrimary(kin)
 { 
 
@@ -41,7 +41,7 @@ void RunAction::BeginOfRunAction(const G4Run* aRun)
   G4cout << "### Run " << aRun->GetRunID() << " start." << G4endl;
 
   fEdep = fEdep2 = 0.;
-    
+
 }
 
 /**
@@ -54,38 +54,37 @@ void RunAction::EndOfRunAction(const G4Run* aRun)
   G4int NbOfEvents = aRun->GetNumberOfEvent();
   if (NbOfEvents == 0) return;
   G4double dNbOfEvents = double(NbOfEvents);
-    
+
   G4ParticleDefinition* particle = fPrimary->GetParticleGun()
-                                          ->GetParticleDefinition();
+    ->GetParticleDefinition();
   G4String partName = particle->GetParticleName();    
   G4double energy   = fPrimary->GetParticleGun()->GetParticleEnergy();
-  
+
   G4double length      = fDetector->GetSize();    
   G4Material* material = fDetector->GetMaterial();
   G4double density     = material->GetDensity();
-   
+
   G4cout << "\n ======================== run summary ======================\n";
-  
+
   G4int prec = G4cout.precision(8);
-  
   G4cout << "\n The run was: " << NbOfEvents << " " << partName << " of "
-         << G4BestUnit(energy,"Energy") << " through " 
-         << G4BestUnit(length,"Length") << " of "
-         << material->GetName() << " (density: " 
-         << G4BestUnit(density,"Volumic Mass") << ")" << G4endl;
+    << G4BestUnit(energy,"Energy") << " through " 
+    << G4BestUnit(length,"Length") << " of "
+    << material->GetName() << " (density: " 
+    << G4BestUnit(density,"Volumic Mass") << ")" << G4endl;
 
-    fEdep /= dNbOfEvents; fEdep2 /= dNbOfEvents;
-    G4double eDepRms = fEdep2 - fEdep*fEdep;
-    if (eDepRms>0.) eDepRms = std::sqrt(eDepRms); else eDepRms = 0.;
-    
- G4cout << "   total energy deposit: " 
-        << G4BestUnit(fEdep, "Energy")
-        << "+/- "<<G4BestUnit(eDepRms,"Energy")
-        <<G4endl;
+  fEdep /= dNbOfEvents; fEdep2 /= dNbOfEvents;
+  G4double eDepRms = fEdep2 - fEdep*fEdep;
+  if (eDepRms>0.) eDepRms = std::sqrt(eDepRms); else eDepRms = 0.;
 
-    G4cout << " ============================================================\n";
-    
-    // reset default precision
- G4cout.precision(prec);
+  G4cout << "   total energy deposit: " 
+    << G4BestUnit(fEdep, "Energy")
+    << " +/- "<<G4BestUnit(eDepRms,"Energy")
+    <<G4endl;
+
+  G4cout << " ============================================================\n";
+
+  // reset default precision
+  G4cout.precision(prec);
 
 }
