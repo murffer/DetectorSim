8d56b5e7d793f226e3d0ba3adf922db73c56bafe
Matthew J. Urffer <matthew.urffer@gmail.com>
2013-11-04 17:09:16 -0500

    Added a PS run
    Added a PS run, hopefully everything is rebuilt.

--- LightValidation/LayeredDetectorCalibration/cleanUp.sh
+++ LightValidation/LayeredDetectorCalibration/cleanUp.sh
@@ -9,7 +9,7 @@ then
 	hadd -f -v 1 EJ426_neutron.root EJ426_neutron-0*.root
 	rm EJ426_neutron-0*.root
 fi
-if test -n "$(find . -maxdepth 1 -name 'EJ426_Gamma-0*.root' -print -quit)"
+if test -n "$(find . -maxdepth 1 -name 'EJ426_gamma-0*.root' -print -quit)"
 then
 	hadd -f -v 1 EJ426_Gamma.root EJ426_Gamma-0*.root
 	rm EJ426_Gamma-0*.root
@@ -21,7 +21,7 @@ then
 	hadd -f -v 1 PSLiF_neutron.root PSLiF_neutron-0*.root
 	rm PSLiF_neutron-0*.root
 fi
-if test -n "$(find . -maxdepth 1 -name 'PSLiF_Gamma-0*.root' -print -quit)"
+if test -n "$(find . -maxdepth 1 -name 'PSLiF_gamma-0*.root' -print -quit)"
 then
 	hadd -f -v 1 PSLiF_Gamma.root PSLiF_Gamma-0*.root
 	rm PSLiF_Gamma-0*.root
